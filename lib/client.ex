439fe2fc7957dfb3dd01070bf704345ebb088d54
Marco Braak <marco@floryn.com>
2025-06-07 08:54:28 +0200

    Handle echo command

--- lib/client.ex
+++ lib/client.ex
@@ -7,20 +7,49 @@ defmodule Client do
     case :gen_tcp.recv(client_socket, 0) do
       {:ok, data} ->
         try do
-          ParseMessage.parse(data)
+          handle_request(
+            ParseMessage.parse(data),
+            client_socket
+          )
         rescue
-          e -> :gen_tcp.close(client_socket)
+          _ -> :gen_tcp.close(client_socket)
         end
-
-        handle_request(client_socket)
 
       {:error, :closed} ->
         nil
+
+      {:error, :enotconn} ->
+        nil
+    end
+
+    run_loop(client_socket)
+  end
+
+  defp handle_request([command | tail], client_socket) do
+    handle_request(
+      String.downcase(command),
+      tail,
+      client_socket
+    )
+  end
+
+  defp handle_request(command, params, client_socket) do
+    case command do
+      "ping" ->
+        ping(params, client_socket)
+
+      "echo" ->
+        echo(params, client_socket)
     end
   end
 
-  defp handle_request(client_socket) do
+  defp ping([], client_socket) do
     :gen_tcp.send(client_socket, "+PONG\r\n")
-    run_loop(client_socket)
+  end
+
+  defp echo([message], client_socket) do
+    length = String.length(message)
+
+    :gen_tcp.send(client_socket, "$#{length}\r\n#{message}\r\n")
   end
 end
