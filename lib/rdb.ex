0962386c508dbe0549bcb3b3e5e763a5f3da25aa
Marco Braak <marco@floryn.com>
2025-06-13 21:45:28 +0200

    Return map

--- lib/rdb.ex
+++ lib/rdb.ex
@@ -1,24 +1,30 @@
 defmodule Rdb do
   def read(path) do
     {:ok, io_device} = File.open(path)
+
     _header = IO.binread(io_device, 9)
-    map = read_sections(io_device)
+    section_type = read_metadatas(io_device)
+
+    if section_type != <<0xFE>> do
+      raise "Expected database subsection"
+    end
+
+    read_database_subsection(io_device)
+    map = read_tables(io_device)
+    read_checksum(io_device)
+
     File.close(io_device)
     map
   end
 
-  defp read_sections(io_device) do
+  defp read_metadatas(io_device) do
     section_type = IO.binread(io_device, 1)
 
-    if section_type != :eof do
-      case section_type do
-        <<0xFA>> -> read_metadata(io_device)
-        <<0xFB>> -> read_tables(io_device)
-        <<0xFE>> -> read_database_subsection(io_device)
-        <<0xFF>> -> read_checksum(io_device)
-      end
-
-      read_sections(io_device)
+    if section_type == <<0xFA>> do
+      read_metadata(io_device)
+      read_metadatas(io_device)
+    else
+      section_type
     end
   end
 
@@ -43,6 +49,12 @@ defmodule Rdb do
   end
 
   defp read_tables(io_device) do
+    section_type = IO.binread(io_device, 1)
+
+    if section_type != <<0xFB>> do
+      raise "Expected tables"
+    end
+
     <<hash_table_size>> = IO.binread(io_device, 1)
     <<_expirations_size>> = IO.binread(io_device, 1)
     <<_value_type>> = IO.binread(io_device, 1)
@@ -57,6 +69,12 @@ defmodule Rdb do
   end
 
   defp read_checksum(io_device) do
+    section_type = IO.binread(io_device, 1)
+
+    if section_type != <<0xFF>> do
+      raise "Expected checksum"
+    end
+
     _checksum = IO.binread(io_device, 8)
   end
 
--- test/test_rdb.exs
+++ test/test_rdb.exs
@@ -2,6 +2,7 @@ defmodule TestRdb do
   use ExUnit.Case, async: true
 
   test "read rdb file" do
-    Rdb.read("test/files/dump.rdb")
+    map = Rdb.read("test/files/dump.rdb")
+    IO.inspect(map)
   end
 end
