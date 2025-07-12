ddf073cfcbefb331850e7696531ec23562a8c987
Marco Braak <marco@floryn.com>
2025-07-12 08:35:36 +0200

    codecrafters submit [skip ci]

--- lib/client.ex
+++ lib/client.ex
@@ -61,6 +61,26 @@ defmodule Client do
     :gen_tcp.send(client_socket, response_data)
   end
 
+  defp handle_request("info", ["replication"], client_socket) do
+    info = ~S"""
+    # Replication
+    role:master
+    connected_slaves:0
+    master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb
+    master_repl_offset:0
+    second_repl_offset:-1
+    repl_backlog_active:0
+    repl_backlog_size:1048576
+    repl_backlog_first_byte_offset:0
+    repl_backlog_histlen:
+    """
+
+    :gen_tcp.send(
+      client_socket,
+      EncodeResp.bulk_string(info)
+    )
+  end
+
   defp handle_request("keys", _, client_socket) do
     keys = Server.Store.keys()
 
