50470db9881a2f7516f33a2f5d3510b314163746
Marco Braak <marco@floryn.com>
2025-07-26 15:59:51 +0200

    Remove inspect from test

--- test/parse_rdb_test.exs
+++ test/parse_rdb_test.exs
@@ -2,7 +2,6 @@ defmodule ParseRdbTest do
   use ExUnit.Case, async: true
 
   test "read rdb file" do
-    entries = ParseRdb.read("test/files/dump.rdb")
-    IO.inspect(entries)
+    ParseRdb.read("test/files/dump.rdb")
   end
 end
